eb3ff35c356ab05bfa5115cf79303978f0566c0b
Pandademic <atharv.gupte510@gmail.com>
2022-02-03 17:20:33 -0500

    remove all gem files

--- lib/unv.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "unv/version"
-
-module Unv
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- lib/unv/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Unv
-  VERSION = "0.1.0"
-end
